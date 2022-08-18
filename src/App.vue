617e21d6c46f8535b11ebb49a6653dd0740c1e95
倪进保 <17756691102@163.com>
2022-08-18 21:57:32 +0800

    完成项目目录结构

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <p>我是App根组件</p>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+@import url("@/assets/css/base.css");
+</style>
